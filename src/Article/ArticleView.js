e3ab31ae7cfc1cb94bd41772595da08d18de6b29
Zander Martineau <zander@martineau.tv>
2017-04-01 14:57:19 +0100

    :hammer: remove redundant brackets

--- src/Article/ArticleView.js
+++ src/Article/ArticleView.js
@@ -2,18 +2,16 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import Header from '../partials/Header'
 
-const ArticleView = (props) => {
-  return (
-    <article className="article h-entry" role="article" itemScope itemType="http://schema.org/Article">
-      <meta itemProp="author" content="{{ site.myname }}" />
+const ArticleView = (props) => (
+  <article className="article h-entry" role="article" itemScope itemType="http://schema.org/Article">
+    <meta itemProp="author" content="Zander Martineau" />
 
-      <Header title={props.title} subtitle={props.subtitle} date={props.date} originalUrl={props.originalUrl} />
+    <Header title={props.title} subtitle={props.subtitle} date={props.date} originalUrl={props.originalUrl} />
 
-      <div className="l-container content content--article e-content" itemProp="articleBody">
-        <ReactMarkdown source={props.body}/>
-      </div>
-    </article>
-  )
-}
+    <div className="l-container content content--article e-content" itemProp="articleBody">
+      <ReactMarkdown source={props.body}/>
+    </div>
+  </article>
+)
 
 export default ArticleView
