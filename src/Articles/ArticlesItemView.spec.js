0762cc1a5e2fea979c722bf0c1301b1bdc243ad1
Zander Martineau <zander@martineau.tv>
2017-04-03 21:24:17 +0100

    :white_check_mark: add more tests

---
+++ src/Articles/ArticlesItemView.spec.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
+import ArticlesItemView from './ArticlesItemView'
+
+test('Article view snapshot test', () => {
+  const component = shallow(
+    <ArticlesItemView
+      title="The title"
+      subtitle="The subtitle"
+      body="The body"
+      date="2017-02-02T00:00+00:00"
+    />
+  )
+  const tree = shallowToJson(component)
+  expect(tree).toMatchSnapshot()
+})
